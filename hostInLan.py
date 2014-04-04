3febf0d27fc3abf66dd7409454b5971b39e66d7b
alazyer <alazyer@gmail.com>
2014-04-04 09:57:59 +0800

    add threading support

--- hostInLan.py
+++ hostInLan.py
@@ -1,16 +1,29 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import sys
 import socket
-
+import threading
 
 # http://stackoverflow.com/questions/10748959/get-hostname-from-ip-address
-def getHost():
-    subnet = '192.168.0.%d'
 
+def getHost(ip):
+    try:
+        print 
+        print socket.gethostbyaddr(ip)  # [name, aliaslist, iplist]
+        print 
+    except:
+        pass
+
+def main():
+    threads = []
+    ip = '192.168.0.%d'
     for i in xrange(1, 255):
-        ip = subnet % i
-        try:
-            print socket.gethostbyaddr(ip)  # [name, aliaslist, iplist]
-        except:
-            print "No host posses %s" % ip
+        t = threading.Thread(target=getHost, args=((ip % i, )))
+        threads.append(t)
+
+    for t in threads:
+        t.start()
+
+if __name__ == '__main__':
+    sys.exit(main())
