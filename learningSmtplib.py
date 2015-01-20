70defb0f15a77e4231af0019464394f4b68d3308
Yuliang Zhang <alazyer@126.com>
2015-01-20 16:17:44 +0800

    发送邮件功能更人性化,可以自己指定邮件服务器等...

--- learningSmtplib.py
+++ learningSmtplib.py
@@ -9,49 +9,53 @@ from email.mime.multipart import MIMEMultipart
 
 # http://www.mkyong.com/python/how-do-send-email-in-python-via-smtplib/
 
-def send_email_using_smtplib():
-    to = 'test@exmaple.com'
-    gmail_user = 'alazyer@gmail.com'
-    gmail_pass = 'pass'
+def send_email_using_smtplib(mail_server, port, from_user, from_password, to_user):
+    to_user = to_user
+    from_user = from_user
+    from_password = from_password
 
-    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server = smtplib.SMTP(mail_server, port)
     server.ehlo()
     server.starttls()
     server.ehlo()
-    server.login(gmail_user, gmail_pass)
-    
-    header = 'To: ' + to + '\n' + 'From: ' + gmail_user + '\n' + 'Subject: Test\n'
+    server.login(from_user, from_password)
+
+    header = 'To: ' + to_user + '\n' + 'From: ' + from_user + '\n' + 'Subject: Test\n'
     print header
     msg = header + '\n This is a test email from %s by using the Python:smtplib modu'
-    server.sendmail(gmail_user, to, msg)
+    server.sendmail(from_user, to_user, msg)
 
     print "Done!"
     server.close()
 
 # http://www.oschina.net/code/snippet_153044_10826
-def send_email():
-    to = '790758591@qq.com'
-    gmail_user = 'alazyer@gmail.com'
-    gmail_pass = 'pass'
+def send_email_multipart(mail_server, port, from_user, from_password, to_user, image_file_path):
+    to_user = to_user
+    from_user = from_user
+    from_password = from_password
 
-    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server = smtplib.SMTP(mail_server, port)
     server.ehlo()
     server.starttls()
     server.ehlo()
-    server.login(gmail_user, gmail_pass)
-    
+    server.login(from_user, from_password)
+
     msg = MIMEMultipart()
-    msg['From'] = gmail_user
-    msg['To'] = to
+    msg['From'] = from_user
+    msg['To'] = to_user
     msg['Subject'] = "This is a test email"
 
     text = MIMEText("This is a test email, using the Python modules: smtplib, email")
-
     msg.attach(text)
 
-    server.sendmail(gmail_user, to, msg.as_string())
-    
+    image = MIMEImage(open(image_file_path, 'rb').read())
+    image.add_header("Content-ID", "<image1>")
+
+    msg.attach(image)
+
+    server.sendmail(from_user, to_user, msg.as_string())
+
     server.close()
-    
+
 if __name__ == '__main__':
     sys.exit(send_email())
